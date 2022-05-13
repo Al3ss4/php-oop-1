fdab4541f509e534fce1e6e95d20bec7d160d4b4
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-13 16:06:38 +0200

    add methods

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
 - è definita una classe ‘Movie’
    => all'interno della classe sono dichiarate delle variabili d'istanza
    => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-*/
 
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
+*/
 
     // - è definita una classe ‘Movie’
     //=> all'interno della classe sono dichiarate delle variabili d'istanza
@@ -18,14 +18,41 @@ class Movie {
 
     public $titolo;
     public $dataDiUscita;
+    public $regista;
 
     // => all'interno della classe è definito un costruttore
-    function __construct($_titolo, $_dataDiUscita){
+    function __construct($_titolo){
         $this -> titolo = $_titolo;
-        $this -> dataDiUscita = $_dataDiUscita;
     }
 
+
+    // => all'interno della classe è definito almeno un metodo
+    public function dataUscita($_dataDiUscita){
+        $this->dataDiUscita = $_dataDiUscita;
+    }
+
+    public function dataUscitaMovie(){
+        return $this->dataDiUscita;
+    }
+
+    public function regista($_regista){
+        $this->regista = $_regista;
+    }
+
+    public function registaMovie(){
+        return $this->regista;
+    }
 
 }
 
 ?>
+
+
+
+
+
+
+
+
+
+
